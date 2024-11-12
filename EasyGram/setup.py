3b13eda21e557c30a31e742ffbfcf140aa65786a
flexyyyapk <147950098+flexyyyapk@users.noreply.github.com>
2024-11-13 00:21:36 +0200

    Update setup.py

--- EasyGram/setup.py
+++ EasyGram/setup.py
@@ -14,5 +14,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.7',
+    url='https://github.com/flexyyyapk/EasyGram/tree/main'
 )
